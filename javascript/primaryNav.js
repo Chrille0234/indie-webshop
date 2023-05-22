a93b936c564039c6edd6cc2d3f2932e57370c9e3
chrille0234 <madsen.chr@hotmail.com>
2023-05-22 12:21:44 +0200

    added content to index

---
+++ javascript/primaryNav.js
@@ -0,0 +1,6 @@
+const burger = document.querySelector('.primaryNav__burger');
+burger.addEventListener('click', () => {
+    const nav = document.querySelector('.primaryNav');
+    nav.classList.toggle('primaryNav--active');
+    
+})
