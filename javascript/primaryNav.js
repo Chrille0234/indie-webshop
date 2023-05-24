545e0985d534c0e070468328269bc8d2f3eb04fb
chrille0234 <madsen.chr@hotmail.com>
2023-05-24 08:25:43 +0200

    all products page added

--- javascript/primaryNav.js
+++ javascript/primaryNav.js
@@ -1,6 +1,5 @@
 const burger = document.querySelector('.primaryNav__burger');
 burger.addEventListener('click', () => {
     const nav = document.querySelector('.primaryNav');
-    nav.classList.toggle('primaryNav--active');
-    
+    nav.classList.toggle('primaryNav--active')
 })
---
+++ javascript/slider.js
@@ -0,0 +1,2 @@
+const sectionItems = document.getElementById("sectionItems")
+sectionItems.style.width = `${sectionItems.children.length*290}px`
